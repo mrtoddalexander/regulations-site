0e00ced77b5fccb0fa24b797b55c2d88eefdb4bd
CM Lubinski <christopher.lubinski@gsa.gov>
2016-06-24 18:18:00 -0400

    -

--- dummy_api/start.sh
+++ dummy_api/start.sh
@@ -11,8 +11,11 @@ do
     curl -X PUT http://localhost:8282/$TAIL -d @$TAIL
 done
 
-# Load a real notice
 mkdir cache
+# Load a real reg
+docker run --rm -it -v $PWD/cache:/app/cache --link core:core eregs/parser pipeline 12 1005 http://core:8080 --only-latest
+
+# Load a real notice
 docker run --rm -it -v $PWD/cache:/app/cache eregs/parser notice_preamble 2016-02749
 docker run --rm -it -v $PWD/cache:/app/cache eregs/parser layers
 docker run --rm -it -v $PWD/cache:/app/cache --link core:core eregs/parser write_to http://core:8080
